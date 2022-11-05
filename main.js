239a1e117ff681f01ae214997d0703bdbd48a7aa
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-11-05 10:59:54 +0100

    added btn start And shuffle the places of cards

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
+const startBtn = document.querySelector(".start span");
 const boxs = document.querySelectorAll(".box");
+let iterables = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+];
 const score = document.querySelector(".score p");
+const name = document.querySelector(".name p");
 let con = 0;
 let twoCardClicked = [];
 let nbrTries = 0;
 
-window.addEventListener("load", () => {
-  boxs.forEach((box) => {
-    box.classList.add("active");
-  });
-  setTimeout(() => {
-    boxs.forEach((box) => {
-      box.classList.remove("active");
-    });
-  }, 2500);
-});
-boxs.forEach((box) => {
-  box.addEventListener("click", onClick);
+startBtn.addEventListener("click", () => {
+  let promptMsg = prompt("Whats Your Name ?", "player");
+  if (promptMsg != null) {
+    name.innerHTML = `Hello: ${promptMsg}`;
+    startBtn.parentElement.remove();
+    startGame();
+  }
 });
 
-function onClick() {
+// Flip a card when pressed
+boxs.forEach((box) => {
+  box.addEventListener("click", flipCard);
+});
+
+function flipCard() {
   if (con < 2 && !this.classList.contains("active")) {
     this.classList.add("active");
     con += 1;
@@ -28,15 +33,32 @@ function onClick() {
     }
   }
 }
+
+function startGame() {
+  boxs.forEach((box) => {
+    // Find out where cards are for a few seconds
+    box.classList.add("active");
+    let i = iterables[Math.floor(Math.random() * iterables.length)];
+    iterables = iterables.filter((e) => e != i);
+    box.style.order = `${i}`;
+  });
+  // flip cards after 4s
+  setTimeout(() => {
+    boxs.forEach((box) => {
+      box.classList.remove("active");
+    });
+  }, 4000);
+}
+
+// Check if two cards are the same
 function verification() {
   setTimeout(() => {
     if (twoCardClicked[0].dataset.card !== twoCardClicked[1].dataset.card) {
+      
       twoCardClicked.forEach((e) => e.classList.remove("active"));
       document.querySelector("#fail").play();
       nbrTries += 1;
       score.innerHTML = `Wrong Tries: ${nbrTries}`;
-      console.log(nbrTries);
-      console.log(score);
     } else {
       document.querySelector("#success").play();
     }
